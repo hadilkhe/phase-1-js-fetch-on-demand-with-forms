c1dcd6386394a2d12030ef71ca303240438db5ad
hadilkhe <121692875+hadilkhe@users.noreply.github.com>
2023-10-01 22:14:39 +0100

    edited asseignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
-const init = () => {
+document.addEventListener('DOMContentLoaded', () => {
+    const movieForm = document.getElementById('movieForm');
+    const movieDetails = document.getElementById('movieDetails');
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+    movieForm.addEventListener('submit', async (event) => {
+      event.preventDefault();
+  
+      const inputField = document.getElementById('searchByID');
+      const movieId = inputField.value;
+  
+      try {
+        const response = await fetch(`http://localhost:3000/movies/${movieId}`);
+        if (response.ok) {
+          const movieData = await response.json();
+          displayMovieDetails(movieData);
+        } else {
+          displayErrorMessage('Movie not found');
+        }
+      } catch (error) {
+        displayErrorMessage('An error occurred');
+      }
+    });
+  
+    function displayMovieDetails(movie) {
+      const titleElement = movieDetails.querySelector('h4');
+      const summaryElement = movieDetails.querySelector('p');
+  
+      titleElement.textContent = movie.title;
+      summaryElement.textContent = movie.summary;
+    }
+  
+    function displayErrorMessage(message) {
+      const titleElement = movieDetails.querySelector('h4');
+      const summaryElement = movieDetails.querySelector('p');
+  
+      titleElement.textContent = 'Error';
+      summaryElement.textContent = message;
+    }
+  });
+  
